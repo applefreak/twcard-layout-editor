cd705c40269aa21df1bb21af4ea61c0add586f9a
Po-Yu Chen <p820316@gmail.com>
2016-09-22 14:54:21 +0800

    [init] Initial commit

---
+++ resources/assets/js/components/TextBox.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="element text draggable" style="background-color: #2299EE; color: white; font-size: 20px;">
+        <div class="input-field" contenteditable="false">
+            {{ msg }}
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                msg: 'hello!!'
+            }
+        }
+    }
+
+</script>
---
+++ resources/views/editor/editor.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-eqiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+<body>
+	<div class="menubar">
+		<button id="add_text">Add Text</button>
+	</div>
+	
+	<div class="property-bar hide">
+		<label for="bg-color">BG Color: </label><input id="bg-color" type="text"></input>
+		<label for="text-color">Text Color: </label><input id="text-color" type="text"></input>
+		<button id="property-done">Done</button>
+	</div>
+
+	<div id="artboard">
+		<textbox></textbox>
+	</div>
+
+	<!-- <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+			  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+			  crossorigin="anonymous"></script> -->
+<!-- 	<script src="https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.css">
+	<script src="js/lib/interact.min.js"></script>
+	<script src="js/lib/jquery.getStyleObject.js"></script>
+
+	<script src="js/shapes.js"></script>
+	<script src="js/property.js"></script>
+	<script src="js/script.js"></script> -->
+
+	<script src="js/app.js"></script>
+</body>
+</html>
