f9cc42e2edd4caec4fe32ce0d19ffb519d5c2790
Poyu Chen <p820316@gmail.com>
2016-10-27 18:40:16 +0800

    [glyphicons] fixed Bootstrap Glyphicons not showing up

--- gulpfile.js
+++ gulpfile.js
@@ -16,4 +16,5 @@ require('laravel-elixir-vue');
 elixir(mix => {
     mix.sass('app.scss')
        .webpack('app.js');
+    mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/','public/fonts/bootstrap');
 });
